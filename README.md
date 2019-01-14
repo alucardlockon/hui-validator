6010c288018f8484eb104fe01fcad4e5ad5310a3
yuwh <alucardlockon3@gmail.com>
2019-01-14 19:39:35 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # hui-validator
 简单的js验证组件
+
+```
+npm i hui-validator
+```
+
+规则示例
+```javascript
+import huiValidator from 'hui-validator'
+
+const rules = [
+ {id:'usernmae',name:'用户名', type:'notnull', max: 10, min: 3, message:'用户名不能为空'},
+ {prop:data.age,name:'number', type:'regexp', regexp: /\d+/, nullable: true }
+]
+
+// 参数分别为验证对象,验证规则，失败回调(用来显示消息)
+huiValidator(valueObj,rules,console.log)
+
+```
+
+## 非空
+`nullable`表示这个属性不能为null
+
+## 内置规则(type)
+
+```
+    非空 notnull
+    整形数字 number
+    小数 float
+    数字或小数 nmmberOrFloat
+    身份证 idCard
+    手机号 phone
+    日期 yyyy-MM-dd date
+    日期时间 yyyy-MM-dd hh:mm:ss
+    扩展: 正则 regexp,需要参数regexp
+```
+
+
+## onValidateFail回调
+```
+三个参数分别为默认消息，验证规则和验证值
+```
---
+++ index.js
@@ -0,0 +1,78 @@
+export default function (valueObj, rules, onValidateFail) {
+  for (var i = 0; i < rules.length; i++) {
+    const rule = rules[i]
+    const value = this.valueObj[rule.prop] != null ? this.valueObj[rule.prop] + '' : ''
+    let reg
+    let msg = ''
+    switch (rule.type) {
+      // 非空
+      case 'notnull':
+        reg = /^.+$/
+        msg = '不能为空'
+        break
+      // 正则
+      case 'regexp':
+        reg = rule.regexp
+        msg = '不符合要求'
+        break
+      // 数字
+      case 'number':
+        reg = /^\d+$/
+        msg = '需要为数字'
+        break
+      // 小数
+      case 'float':
+        reg = /^\d+[.]\d+$/
+        msg = '需要为小数'
+        break
+      // 数字
+      case 'nmmberOrFloat':
+        reg = /^\d+([.]\d+)?$/
+        msg = '需要为整数或小数'
+        break
+      // 身份证
+      case 'idCard':
+        reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/
+        msg = '需要符合身份证格式'
+        break
+      // 手机号
+      case 'phone':
+        reg = /^1[34578]\d{9}$/
+        msg = '需要为手机号'
+        break
+      // 日期 yyyy-MM-dd
+      case 'date':
+        reg = /^\d{4}-\d{2}-\d{2}$/
+        msg = '需要为日期(yyyy-MM-dd)'
+        break
+      // 日期时间 yyyy-MM-dd hh:mm:ss
+      case 'datetime':
+        reg = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/
+        msg = '需要为日期(yyyy-MM-dd hh:mm:ss)'
+        break
+    }
+
+    // 可为空
+    if (rule.nullable && value === '') {
+      return true
+    }
+
+    // 长度检查
+    if (rule.max && rule.max < value.length) {
+      onValidateFail(rule.name + '的最大长度为' + rule.max, rule, value)
+      return false
+    }
+    if (rule.min && rule.min > value.length) {
+      onValidateFail(rule.name + '的最大长度为' + rule.max, rule, value)
+      return false
+    }
+
+    // 验证
+    if (!reg.test(value)) {
+      onValidateFail(rule.message ? rule.message : (rule.name + msg), rule, value)
+      return false
+    }
+
+  }
+  return true;
+}
